e0aea7246f833e258771e1a9464e68ec9b8522c8
Yash Shetye <87604378+Yash-S-Shetye@users.noreply.github.com>
2023-04-05 15:46:18 -0400

    Create servos_test.c

---
+++ servos_test.c
@@ -0,0 +1,46 @@
+// Test code for testing continuous rotation servos
+
+#include "simpletools.h" // Include simpletools
+#include "servo.h" // Include servo library
+int main() // Main function
+{
+  // Forward motion
+  servo_speed(2, 100);
+  servo_speed(3, -100); 
+  pause(2000);
+    
+  // Stop 
+  servo_speed(2, 0);
+  servo_speed(3, 0); 
+  pause(2000);
+    
+  //Left
+  servo_speed(2, 100);
+  servo_speed(3, 100); 
+  pause(2000);
+    
+  // Stop
+  servo_speed(2, 0);
+  servo_speed(3, 0); 
+  pause(2000);
+    
+  // Right
+  servo_speed(2, -100);
+  servo_speed(3, -100); 
+  pause(2000);
+    
+  // Stop
+  servo_speed(2, 0);
+  servo_speed(3, 0); 
+  pause(2000);
+    
+  // Backward
+  servo_speed(2, -100);
+  servo_speed(3, 100); 
+  pause(2000);
+    
+  // Stop
+  servo_spin(2, 0);
+  servo_spin(3, 0); 
+  pause(2000);
+}      
