b5585b7f3536e42cdd13bd873f3c2ae30f9c019e
Yash Shetye <87604378+Yash-S-Shetye@users.noreply.github.com>
2023-04-06 10:08:21 -0400

    Update servos_test.c

--- servos_test.c
+++ servos_test.c
@@ -4,43 +4,46 @@
 #include "servo.h" // Include servo library
 int main() // Main function
 {
-  // Forward motion
-  servo_speed(2, 100);
-  servo_speed(3, -100); 
-  pause(2000);
+  while(1) {
     
-  // Stop 
-  servo_speed(2, 0);
-  servo_speed(3, 0); 
-  pause(2000);
+    // Forward motion
+    servo_speed(2, 100);
+    servo_speed(3, -100); 
+    pause(2000);
     
-  //Left
-  servo_speed(2, 100);
-  servo_speed(3, 100); 
-  pause(2000);
+    // Stop 
+    servo_speed(2, 0);
+    servo_speed(3, 0); 
+    pause(2000);
     
-  // Stop
-  servo_speed(2, 0);
-  servo_speed(3, 0); 
-  pause(2000);
+    // Left
+    servo_speed(2, 100);
+    servo_speed(3, 100); 
+    pause(2000);
     
-  // Right
-  servo_speed(2, -100);
-  servo_speed(3, -100); 
-  pause(2000);
+    // Stop
+    servo_speed(2, 0);
+    servo_speed(3, 0); 
+    pause(2000);
     
-  // Stop
-  servo_speed(2, 0);
-  servo_speed(3, 0); 
-  pause(2000);
+    // Right
+    servo_speed(2, -100);
+    servo_speed(3, -100); 
+    pause(2000);
     
-  // Backward
-  servo_speed(2, -100);
-  servo_speed(3, 100); 
-  pause(2000);
+    // Stop
+    servo_speed(2, 0);
+    servo_speed(3, 0); 
+    pause(2000);
     
-  // Stop
-  servo_spin(2, 0);
-  servo_spin(3, 0); 
-  pause(2000);
+    // Backward
+    servo_speed(2, -100);
+    servo_speed(3, 100); 
+    pause(2000);
+    
+    // Stop
+    servo_spin(2, 0);
+    servo_spin(3, 0); 
+    pause(2000);
+  }
 }      
