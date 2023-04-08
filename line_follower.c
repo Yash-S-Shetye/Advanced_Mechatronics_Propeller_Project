850675cb145ae0f954ebf521644f6841eacb6cc6
Yash Shetye <87604378+Yash-S-Shetye@users.noreply.github.com>
2023-04-08 13:23:37 -0400

    Update line_follower.c

--- line_follower.c
+++ line_follower.c
@@ -6,29 +6,32 @@
 #define LEFT_SENSOR 4 // Define pin number for left sensor
 #define RIGHT_SENSOR 8 // Define pin number for right sensor
 
+int leftServo = 2;
+int rightServo = 3;
+
 
 int main() {
 
   while (1) { // Loop infinitely
-    int left_val = input(LEFT_SENSOR); // Read value from left sensor
-    int right_val = input(RIGHT_SENSOR); // Read value from right sensor
+    int left_IR = input(LEFT_SENSOR); // Read value from left sensor
+    int right_IR = input(RIGHT_SENSOR); // Read value from right sensor
     
-    if (left_val == 1 && right_val == 0) { // If left sensor detects line and right sensor does not
+    if (left_IR == 1 && right_IR == 0) { // If left sensor detects line and right sensor does not
       // Turn left
-      servo_speed(2, 100); // Set left motor speed to maximum in reverse direction
-      servo_speed(3, -100); // Set right motor speed to maximum in forward direction
-    } else if (left_val == 0 && right_val == 1) { // If right sensor detects line and left sensor does not
+      servo_speed(leftSevo, 100); // Set left motor speed to maximum in reverse direction
+      servo_speed(rightServo, -100); // Set right motor speed to maximum in forward direction
+    } else if (left_IR == 0 && right_IR == 1) { // If right sensor detects line and left sensor does not
       // Turn right
-      servo_speed(2, -100); // Set left motor speed to maximum in forward direction
-      servo_set(3, 100); // Set right motor speed to maximum in reverse direction
-    } else if (left_val == 0 && right_val == 0) { // If both sensors do not detect line
+      servo_speed(leftServo, -100); // Set left motor speed to maximum in forward direction
+      servo_set(rightServo, 100); // Set right motor speed to maximum in reverse direction
+    } else if (left_IR == 0 && right_IR == 0) { // If both sensors do not detect line
       // Move forward
-      servo_speed(2, 100); // Set left motor speed to maximum in forward direction
-      servo_speed(3, 100); // Set right motor speed to maximum in forward direction
+      servo_speed(leftServo, 100); // Set left motor speed to maximum in forward direction
+      servo_speed(rightServo, 100); // Set right motor speed to maximum in forward direction
     } else { // If both sensors detect line
       // Stop
-      servo_set(2, 0); // Set left motor speed to zero
-      servo_set(3, 0); // Set right motor speed to zero
+      servo_set(leftServo, 0); // Set left motor speed to zero
+      servo_set(leftServo, 0); // Set right motor speed to zero
     }
   }
 }
