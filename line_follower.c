d5e15664e02f1a69e185eca90c41085dc50a626f
Yash Shetye <87604378+Yash-S-Shetye@users.noreply.github.com>
2023-04-12 16:11:56 -0400

    Update line_follower.c

--- line_follower.c
+++ line_follower.c
@@ -6,8 +6,8 @@
 #define LEFT_SENSOR 4 // Define pin number for left sensor
 #define RIGHT_SENSOR 8 // Define pin number for right sensor
 
-int leftServo = 2;
-int rightServo = 3;
+int leftServo = 16;
+int rightServo = 17;
 
 
 int main() {
@@ -18,16 +18,16 @@ int main() {
     
     if (left_IR == 1 && right_IR == 0) { // If left sensor detects line and right sensor does not
       // Turn left
-      servo_speed(leftSevo, 100); // Set left motor speed to maximum in reverse direction
-      servo_speed(rightServo, -100); // Set right motor speed to maximum in forward direction
+      servo_speed(leftServo, -50); // Set left motor speed to maximum in reverse direction
+      servo_speed(rightServo, -50); // Set right motor speed to maximum in forward direction
     } else if (left_IR == 0 && right_IR == 1) { // If right sensor detects line and left sensor does not
       // Turn right
-      servo_speed(leftServo, -100); // Set left motor speed to maximum in forward direction
-      servo_set(rightServo, 100); // Set right motor speed to maximum in reverse direction
+      servo_speed(leftServo, 50); // Set left motor speed to maximum in forward direction
+      servo_set(rightServo, 50); // Set right motor speed to maximum in reverse direction
     } else if (left_IR == 0 && right_IR == 0) { // If both sensors do not detect line
       // Move forward
-      servo_speed(leftServo, 100); // Set left motor speed to maximum in forward direction
-      servo_speed(rightServo, 100); // Set right motor speed to maximum in forward direction
+      servo_speed(leftServo, 50); // Set left motor speed to maximum in forward direction
+      servo_speed(rightServo, -50); // Set right motor speed to maximum in forward direction
     } else { // If both sensors detect line
       // Stop
       servo_set(leftServo, 0); // Set left motor speed to zero
