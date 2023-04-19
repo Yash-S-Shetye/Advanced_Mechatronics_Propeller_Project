dcdc06c9043b2f4ca6a3ea3cb15d4b4d7a5bb4a7
Yash Shetye <87604378+Yash-S-Shetye@users.noreply.github.com>
2023-04-19 16:24:21 -0400

    Update final_main.c

--- final_main.c
+++ final_main.c
@@ -1,5 +1,5 @@
 // Main Code
-
+``
 #include "simpletools.h"                      // Include simple tools
 #include "servo.h"                            // Include servo library
 #include "ping.h"                             // Include ping sensor library
@@ -9,70 +9,71 @@
 int white = 0;
 int black = 1;
 
+int c_intersection=0; // Initializing the encounterted intersections count
+int pickup, dropoff; // Keeping track of pickup and dropoff locations
+int ob_distance=20; // Safe distance from obstacles for detection
+int obj_distance=10; // Distance from detected object
+int pd_distance; // Variable for storing total distance frompickup to dropoff point
+int distance2;
+
 // Initializing parameters for lcd display
 const int BO = 17;
 const int ON  = 22;
 const int CLR = 12;
 const int CRT = 13;
 
-int c_intersection=0;
-bool slowdownflag = false; // Flag for slowing down bot in lanes
-int pickup, dropoff; // Keeping track of pickup and dropoff locations
-int ob_distance=10; // Safe distance from obstacles for detection
-int obj_distance=20; // Distance from detected object
-int pd_distance; // Variable for storing total distance frompickup to dropoff point
-
 static volatile int distance; // Variable for storing distance from obstacle on cog1
 
-// Initializing pin values for second ultrasonic sensor
-const int trigPin = 1;
-const int echoPin = 2;
+
 
 // Variables being changed on different cogs
 static volatile long duration;
-static volatile int distance2;
+
 static volatile int led;
 static volatile bool ledflag;
 
 // Global vars for cogs to share
 unsigned int stack[40 + 25]; // Stack vars for cog1`for obstacle detection ping sensor
-unsigned int stack1[40 + 25]; // Stack vars for cog2 for led indicating intersections encountered
-unsigned int stack2[40 + 25]; // Stack vars for cog3`for object detecting ultrasonic sensor
+unsigned int stack1[40 + 25]; // Stack vars for cog2`for object detecting ultrasonic sensor
+unsigned int stack2[40 + 25]; // Stack vars for cog3 for led indicating intersections encountered
 
 
 serial *lcd; // Creating object for lcd
 
-bool finish = false;
-bool localfinish = false;
-
 // Initializing variables for servos, IR sensor and obstacle detecting led
-const int leftWheel=16;
-const int rightWheel=17;
-const int ultrasonic=11;
-const int IR_ML=4;
-const int IR_MR=8;
-const int led2=7;
+const int leftWheel=16;  // Left servo motor
+const int rightWheel=17; // Right servo motor
+const int ultrasonic=11; // Ping sensor
+const int IR_ML=4; // Left IR sensor
+const int IR_MR=8; // Right IR sensor
+const int led2=7; // Led for detecting dynamic obstacles
 
+// Initializing pin values for second ultrasonic sensor
+const int trigPin = 1;
+const int echoPin = 2;
 
-// Declaring functions being used
-void avoid_obstacle();
-void obstacle(void *dist);
-void object(void *dist2);
-void led_blink(void *ledPin);
-void drive(char i);
-void lcd_display(char disp);
-bool linefollowing();
-void test();
-void init();
+bool slowdownflag = false; // Flag for slowing down bot in lanes
+bool localfinish = false; // Flag for line following
+
+// Declaring all functions being used
+void avoid_obstacle(); // Function for avoiding obstacles
+void obstacle(void *dist); // Function for getting distance from obstacle defined on cog1
+void object(); // Function for detecting objects
+void led_blink(void *ledPin); // Function for blinking intersection indicating led defined on cog2
+void drive(char i); // Function for driving servo motors
+void lcd_display(char disp); // Function for displaying messages on lcd screen
+bool linefollowing(); // Function for line following
+void init(); // Function for initializing all values
 
 // Defining initializing function
 void init() {
+  // Initializing intersection indicating led and lcd screen
   led=9;
   ledflag=false;
   lcd = serial_open(12, 12, 0, 9600);
-  writeChar(lcd, BO);
-  writeChar(lcd, ON);
-  writeChar(lcd, CLR);
+  writeChar(lcd, BO); // Turn on lcd backlight
+  writeChar(lcd, ON); // Turn on lcd display
+  writeChar(lcd, CLR); // Clear screen
   pause(5);
   dprint(lcd, "Initialized!!");
   pause(1000);
@@ -83,11 +84,11 @@ void init() {
 void drive(char i) {
   int speed;
   if(slowdownflag==true)
-    speed=30;
+    speed=30; // Speed of bot in lanes A and B
   else
-    speed=50;
+    speed=50; // Normal speed of bot
   switch(i){
-  // f, b, l, r, s means forward, backward, left, right, and stop
+    // f, b, l, r, s means forward, backward, left, right, and stop
     case 'f':servo_speed(leftWheel, speed);servo_speed(rightWheel, -speed);pause(20);break;
     case 'b':servo_speed(leftWheel, -speed);servo_speed(rightWheel, speed);pause(20);break;
     case 'l':servo_speed(leftWheel, -speed);servo_speed(rightWheel, -speed);pause(20);break;
@@ -128,34 +129,32 @@ void obstacle(void *dist) {
 }
 
 // Defining function for measuring distance from object
-void object(void *dist2) {
-  while(1) {
-    low(trigPin);
-    pause(2);
-    high(trigPin);
-    pause(10);
-    low(trigPin);
-    duration = pulse_in(echoPin, 1);
-    distance2 = duration * 0.034/2;
-    pause(200);
-  }
-}
+void object() {
+  low(trigPin);  // Clear the trigPin
+  pause(2);
+  high(trigPin);  // Set the trigPin on HIGH state for 10 micro seconds
+  pause(10);
+  low(trigPin); // Read the echoPin, returns the sound wave travel time in microseconds
+  duration = pulse_in(echoPin, 1); // Calculating the distance
+  distance2 = duration * 0.034/2; // Get the distance value in cm
+}  
 
 // Defining function for blinking led when bot encounters intersection
 void led_blink(void *ledPin) {
   while(1) {
     if(ledflag==true) {
-      high(led);
-      pause(500);
-      low(led);
-      pause(500);
-      ledflag=false;
+      high(led); // Turn on led
+      pause(500); // Pause for 500 milliseconds
+      low(led); // Turn off led
+      pause(500); // Pause for 500 milliseconds
+      ledflag=false; // Changing flag value to it's intialized value
     } 
   }
 }
 
 // Function for checking for object to be picked up
 bool isobject() {
+  object();
   if(distance2<obj_distance) {
     return true;} 
   else {
@@ -170,11 +169,10 @@ bool isobstacle() {
     return false;}
 }
 
-
 //Defining lcd display function
 void lcd_display(char disp) {
     switch(disp) {
-      case 'o':dprint(lcd, "Object Detected");  // i - Intersection detected
+      case 'o':dprint(lcd, "Object Detected");
                pause(1000);
                writeChar(lcd, CLR);
                break;
@@ -182,13 +180,9 @@ void lcd_display(char disp) {
                pause(2000);
                writeChar(lcd, CLR);
                dprint(lcd, "Distance=%d cm",pd_distance);
-               pause(2000);
+               pause(4000);
                writeChar(lcd, CLR);
                break;
-      /*case 'd':dprint(lcd, "Distance=%d cm",pd_distance);
-               pause(2000);
-               writeChar(lcd, CLR);
-               break;*/
       default:dprint(lcd, "Error");
               writeChar(lcd, CLR);
               break;
@@ -207,10 +201,10 @@ void avoid_obstacle(){
       drive('s');
     }
     low(led2);                
-    if(isintersection && c_itsc==0) { //if meet intersection
-      ledflag=true;
+    if(isintersection && c_itsc==0) { // If meet intersection
+      ledflag=true; // Changing flag value to turn on intersection indicating led
       drive('f');pause(500); 
-      drive('r');pause(1000); //turn 90
+      drive('r');pause(1000); // Turn right
       slowdownflag=true;
       c_itsc++;
     }
@@ -224,7 +218,7 @@ void avoid_obstacle(){
     else if(isintersection && c_itsc==2){
       ledflag=true;
       c_intersection++;
-      if(c_intersection==5) {
+      if(c_intersection==5) { // If reaches last intersection
         drive('f');pause(1000); // Move straight
         ob_flag=true;
         localfinish=true;
@@ -248,9 +242,8 @@ void avoid_obstacle(){
 int main()
 {
   init();
-  cogstart(&obstacle, NULL, stack, sizeof(stack));      // Starting cog for detecting obstacles
-  cogstart(&object, NULL, stack1, sizeof(stack1));      // Starting cog for blinking led
-  cogstart(&led_blink, NULL, stack2, sizeof(stack2));   // Starting cog for detecting objects
+  cogstart(&obstacle, NULL, stack, sizeof(stack));      // Starting cog1 for detecting obstacles
+  cogstart(&led_blink, NULL, stack2, sizeof(stack2));   // Starting cog3 for blinking led
 
   // 1 go through the center course
   localfinish=false;
@@ -258,11 +251,10 @@ int main()
     if(!linefollowing()){     //if meets intersection
       ledflag=true;
       c_intersection++;
-      //lcd_display('i');
       if(c_intersection<5 && isobstacle()==true) { // If detects obstacle
         drive('f');pause(500);
-        drive('l');pause(1000);
-        avoid_obstacle();
+        drive('l');pause(1000); // Turn left
+        avoid_obstacle(); // Calling obstacle avoidance function
       }
       else { 
         if(c_intersection==5) { // If reaches last intersection
@@ -283,20 +275,19 @@ int main()
   bool isintersection=false;
   while(!localfinish) {
     isintersection=!linefollowing();
-    while(c_p !=0 && isobstacle() == true) {
-      high(led2);
+    while(c_p !=0 && isobstacle() == true) { // Checking for dynamic obstacles
+      high(led2); // Turn on led
       drive('s');
     }
-    low(led2);      
-    if(isintersection && c_p==0) {
+    low(led2); // Turn off led     
+    if(isintersection && c_p==0) { // First pick up point
       ledflag=true;
       drive('f');pause(500);
-      drive('r');pause(1000); 
-      if(isobject()==true){
-        //drive('f');pause(500);
-        drive('s');
+      drive('r');pause(1000); // Turn right
+      if(isobject()==true){ // Checking for objects at pick up points
+        drive('s');  // Stop
         pause(1000);
-        lcd_display('o');
+        lcd_display('o'); // Display object detected on lcd screen
         drive('f');pause(100);
         pickup=5;}
           
@@ -307,9 +298,9 @@ int main()
       ledflag=true;
       if(isobject()==true){
         drive('f');pause(500);
-        drive('s');
+        drive('s'); // Stop
         pause(1000);
-        lcd_display('o');
+        lcd_display('o'); // Display object detected on lcd screen
         drive('f');
         pause(1000);
         pickup=5-c_p;
@@ -322,19 +313,20 @@ int main()
     else if(isintersection && c_p==4) {
       ledflag=true;
       if(isobject()==true){
-        drive('f');pause(500);
-        drive('s');
+        //drive('f');pause(500);
+        drive('s'); // Stop
         pause(1000);
-        lcd_display('o');
+        lcd_display('o'); // Display object detected on lcd screen
         //drive('f');pause(1000);
         pickup=1;
       }
       slowdownflag=false; 
-      //drive('f');pause(500);
+      drive('f');pause(500);
       drive('r');pause(1000);    
       c_p++;       
     }
     else if(isintersection && c_p==5){   //cross the center course
+      ledflag=true;
       drive('f');pause(1000);
       localfinish=true;
     }
@@ -345,26 +337,26 @@ int main()
   localfinish=false;
   while(!localfinish){
     isintersection=!linefollowing();
-    while(c_d != 0 && isobstacle() == true) {
-      high(led2);
+    while(c_d != 0 && isobstacle() == true) { // Checking for dynamic obstacles
+      high(led2); // Turn on led
       drive('s');
     }
-    low(led2);      
-    if(isintersection && c_d==0){
+    low(led2); // Turn off led     
+    if(isintersection && c_d==0){ // First drop offpoint
       ledflag=true;
       drive('f');pause(500);
-      drive('r');pause(1000);
+      drive('r');pause(1000); // Turn right
       slowdownflag=true;
       c_d++;
       if(isobject()==true){
         drive('s');pause(1000);
         dropoff=1;
-        break;  
+        break;  // End program
       }
     }
     else if(isintersection && isobject()==true){
       ledflag=true;
-      // arrived drop point
+      // Arrived drop point
       if(isobject()==true){
         drive('f');pause(500);
         drive('s');pause(1000);
@@ -380,8 +372,8 @@ int main()
     }      
   }
 
-  pd_distance=40*(pickup+dropoff);
+  pd_distance=40*(pickup+dropoff); // Calculating total distance from pick up and drop off point
   pause(1000);
-  lcd_display('e');
-  //lcd_display('d');
+  lcd_display('e'); // Displaying object placed at drop off point and the total distance from pick up and drop off point
+  pause(1000);
 }
